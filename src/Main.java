b71b73229431b44684429c620262b49d7827d084
Ozge <ozgeozler_93@hotmail.com>
2023-11-30 01:37:52 +0300

    Odev1 icin kod.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java101 Patikasi");
+        System.out.print("Basliyoruz,\n");
+        System.out.println("Hadi!");
+    }
+}
